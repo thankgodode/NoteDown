649eb55e5de866a769754e900e28b818335ba1f5
Thankgod Ode <thankgodode535@gmail.com>
2025-09-08 04:42:43 +0100

    Modularized the client side API handling of convertToWord implementation

--- client/utils/convertToWord.js
+++ client/utils/convertToWord.js
@@ -1,8 +1,9 @@
 import axios from "axios"
+import { PermissionsAndroid } from "react-native"
 
 import RNFS from "react-native-fs"
 
-async function convertToWord(html) {
+async function convertToWord(data) {
     try {
         const response = await fetch("http://192.168.43.38:5000/api/toDocx",
             {
@@ -10,15 +11,57 @@ async function convertToWord(html) {
                 headers: {
                     "Content-Type":"application/json"
                 },
-                body:JSON.stringify({content:html})
+                body:JSON.stringify({html:data.content})
             }
         )
 
         const result = await response.json()
 
-        console.log("Result: ", result)
+        const permission = await requestPermission()
+
+        if (permission === true) {
+            writeToFile(data.title,result.message)
+        }
+        return
     } catch (error) {
         console.error("Sorry an error just occured ", error)
+    }
+}
+
+const requestPermission = async() => {
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            {
+                title: "Storage Permission",
+                message: "App needs access to your storate to write files.",
+                buttonNeutral: "Ask me later",
+                buttonNegative: "Cancel",
+                buttonPositive: "OK"
+            }
+        );
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("Permission granted!")
+            return true
+        } else {
+            console.log("Permission not granted...")
+            return false
+        }
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+const writeToFile = async(fileName,buffer) => {
+    const path = RNFS.ExternalDirectoryPath + `//${fileName}.docx`
+    console.log("Path ", path)
+    
+    try {
+        await RNFS.writeFile(path, buffer, "base64")
+        console.log("File written at: ", path)
+    } catch (error) {
+        console.error("File write error: ", error)
     }
 }
 
